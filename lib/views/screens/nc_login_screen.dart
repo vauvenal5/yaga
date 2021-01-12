96bfd70eaa5779a53e395d3fcd0280a60267a14f
vauvenal5 <vauvenal5@gmail.com>
2021-01-12 14:06:16 +0100

    fix login being navigated to wrong page

--- lib/views/screens/nc_login_screen.dart
+++ lib/views/screens/nc_login_screen.dart
@@ -16,44 +16,44 @@ class NextCloudLoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Sign in..."),
-        ),
-        body: WebView(
-          key: UniqueKey(),
-          initialUrl: this._url.toString(),
-          userAgent: "Nextcloud Yaga",
-          javascriptMode: JavascriptMode.unrestricted,
-          onWebViewCreated: (WebViewController webViewController) {
-            webViewController.loadUrl(
-                this._url.toString() + "/index.php/login/flow",
-                headers: <String, String>{"OCS-APIREQUEST": "true"});
-          },
-          navigationDelegate: (NavigationRequest request) {
-            if (request.url.startsWith("nc")) {
-              //string of type: nc://login/server:<server>&user:<loginname>&password:<password>
-              Map<String, String> ncParas = request.url
-                  .split("nc://login/")[1]
-                  .split("&")
-                  .map((e) => e.split(":"))
-                  .map((e) => <String, String>{e.removeAt(0): e.join(":")})
-                  .reduce((value, element) {
-                value.addAll(element);
-                return value;
-              });
+      appBar: AppBar(
+        title: Text("Sign in..."),
+      ),
+      body: WebView(
+        key: UniqueKey(),
+        userAgent: "Nextcloud Yaga",
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (WebViewController webViewController) {
+          webViewController.loadUrl(
+              this._url.toString() + "/index.php/login/flow",
+              headers: <String, String>{"OCS-APIREQUEST": "true"});
+        },
+        navigationDelegate: (NavigationRequest request) async {
+          if (request.url.startsWith("nc")) {
+            //string of type: nc://login/server:<server>&user:<loginname>&password:<password>
+            Map<String, String> ncParas = request.url
+                .split("nc://login/")[1]
+                .split("&")
+                .map((e) => e.split(":"))
+                .map((e) => <String, String>{e.removeAt(0): e.join(":")})
+                .reduce((value, element) {
+              value.addAll(element);
+              return value;
+            });
 
-              getIt.get<NextCloudManager>().loginCommand(NextCloudLoginData(
-                  Uri.parse(ncParas[NextCloudLoginDataKeys.server]),
-                  Uri.decodeComponent(ncParas[NextCloudLoginDataKeys.user]),
-                  ncParas[NextCloudLoginDataKeys.password]));
+            getIt.get<NextCloudManager>().loginCommand(NextCloudLoginData(
+                Uri.parse(ncParas[NextCloudLoginDataKeys.server]),
+                Uri.decodeComponent(ncParas[NextCloudLoginDataKeys.user]),
+                ncParas[NextCloudLoginDataKeys.password]));
 
-              Navigator.popUntil(
-                  context, ModalRoute.withName(YagaHomeScreen.route));
-              return NavigationDecision.prevent;
-            }
+            Navigator.popUntil(
+                context, ModalRoute.withName(YagaHomeScreen.route));
+            return NavigationDecision.prevent;
+          }
 
-            return NavigationDecision.navigate;
-          },
-        ));
+          return NavigationDecision.navigate;
+        },
+      ),
+    );
   }
 }

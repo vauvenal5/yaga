f631215a7b7d101327241fe6f1e93d4528a7aab5
vauvenal5 <vauvenal5@gmail.com>
2023-11-02 17:46:11 +0100

    refactor background bridge

--- lib/utils/forground_worker/bridges/file_manager_bridge.dart
+++ lib/utils/forground_worker/bridges/file_manager_bridge.dart
@@ -1,13 +1,12 @@
+import 'package:rx_command/rx_command.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:yaga/managers/file_manager/file_manager.dart';
 import 'package:yaga/managers/file_service_manager/media_file_manager.dart';
-import 'package:yaga/model/fetched_file.dart';
 import 'package:yaga/utils/background_worker/background_worker.dart';
 import 'package:yaga/utils/forground_worker/foreground_worker.dart';
-import 'package:yaga/utils/forground_worker/messages/file_list_message.dart';
-import 'package:yaga/utils/forground_worker/messages/file_update_msg.dart';
-import 'package:yaga/utils/forground_worker/messages/files_action/files_action_done.dart';
 import 'package:yaga/utils/forground_worker/messages/image_update_msg.dart';
+
+import 'package:yaga/utils/forground_worker/messages/message.dart';
 
 //todo: slowly deprecate bridges; this is logic which belongs into the fileManager
 class FileManagerBridge {
@@ -22,32 +21,14 @@ class FileManagerBridge {
     this._mediaFileManager,
     this._backgroundWorker,
   ) {
-    _backgroundWorker.isolateResponseCommand
-        .where((event) => event is ImageUpdateMsg)
-        .map((event) => event as ImageUpdateMsg)
-        .listen((msg) => _fileManager.updateImageCommand(msg.file));
-
-    _worker.isolateResponseCommand
-        .mergeWith([_backgroundWorker.isolateResponseCommand])
-        .where((event) => event is FetchedFile)
-        .map((event) => event as FetchedFile)
-        .listen((event) => _fileManager.fetchedFileCommand(event));
-
-    _worker.isolateResponseCommand
-        .where((event) => event is FileListMessage)
-        .map((event) => event as FileListMessage)
-        .listen((event) => _fileManager.updateFilesCommand(event));
-
-    _worker.isolateResponseCommand
-        .mergeWith([_backgroundWorker.isolateResponseCommand])
-        .where((event) => event is FilesActionDone)
-        .map((event) => event as FilesActionDone)
-        .listen((event) => _fileManager.filesActionDoneCommand(event));
-
-    _backgroundWorker.isolateResponseCommand
-        .where((event) => event is FileUpdateMsg)
-        .map((event) => event as FileUpdateMsg)
-        .listen((event) => _fileManager.fileUpdateMessage(event));
+    _registerWorkerMessageWithTransformation(
+      (ImageUpdateMsg msg) => msg.file,
+      _fileManager.updateImageCommand,
+    );
+    _registerWorkerMessage(_fileManager.fetchedFileCommand);
+    _registerWorkerMessage(_fileManager.filesActionDoneCommand);
+    _registerWorkerMessage(_fileManager.updateFilesCommand);
+    _registerWorkerMessage(_fileManager.fileUpdateMessage);
 
     _fileManager.fetchFileListCommand
         .where((event) => event.uri.scheme != _mediaFileManager.scheme)
@@ -55,5 +36,19 @@ class FileManagerBridge {
 
     _fileManager.sortFilesListCommand
         .listen((value) => _worker.sendRequest(value));
+  }
+
+  _registerWorkerMessageWithTransformation<T extends Message, P>(
+      P Function(T) transformation, RxCommand<P, P> command) {
+    _worker.isolateResponseCommand
+        .mergeWith([_backgroundWorker.isolateResponseCommand])
+        .where((event) => event is T)
+        .map((event) => event as T)
+        .map(transformation)
+        .listen((msg) => command(msg));
+  }
+
+  _registerWorkerMessage<T extends Message>(RxCommand<T, T> command) {
+    _registerWorkerMessageWithTransformation((T msg) => msg, command);
   }
 }
